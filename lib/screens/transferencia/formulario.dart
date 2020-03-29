cb95a9886d24ee4974696a9d4691df7707ae7830
fabiovalinhos <fabiovalinhos@gmail.com>
2020-03-29 09:36:54 -0300

    Refatoramento do código e finalização do projeto

---
+++ lib/screens/transferencia/formulario.dart
@@ -0,0 +1,66 @@
+import 'package:bytebank/components/editor.dart';
+import 'package:bytebank/models/transferencia.dart';
+import 'package:flutter/material.dart';
+
+const _tituloAppBar = "Criando Transferência";
+const _rotuloCampoValor = "Valor";
+const _dicaCampoValor = "0.00";
+const _rotuloCampoConta = "Número da Conta";
+const _dicaCampoConta = "0000";
+const _textoBotaoConfirmar = "Confirmar";
+
+class FormularioTransferencia extends StatefulWidget {
+  @override
+  _FormularioTransferenciaState createState() => _FormularioTransferenciaState();
+}
+
+class _FormularioTransferenciaState extends State<FormularioTransferencia> {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_tituloAppBar),
+      ),
+      body: SingleChildScrollView(
+              child: Column(
+          children: <Widget>[
+            Editor(
+              controlador: _controladorCampoNumeroConta,
+              rotulo: _rotuloCampoConta,
+              dica: _dicaCampoConta,
+            ),
+            Editor(
+              controlador: _controladorCampoValor,
+              rotulo: _rotuloCampoValor,
+              dica: _dicaCampoValor,
+              icone: Icons.monetization_on,
+            ),
+            RaisedButton(
+              child: Text(_textoBotaoConfirmar),
+              onPressed: () => _criaTranferencia(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _criaTranferencia(BuildContext context) {
+    final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double valor = double.tryParse(_controladorCampoValor.text);
+
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+
+      // Debug print sempre espera uma string, então isto é uma técnica para inserir uma variável dentro de uma string
+      debugPrint('$transferenciaCriada');
+      debugPrint("Criando transferência");
+      Navigator.pop(context, transferenciaCriada);
+    }
+  }
+}
